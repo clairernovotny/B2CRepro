b76657e981909213f05e43fe0eeeb4efc8e85140
Oren Novotny <oren@novotny.org>
2016-10-05 15:11:45 -0400

    Add B2C

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public async Task SignIn()
+        {
+            var authenticationProperties = new AuthenticationProperties { RedirectUri = "/" };
+
+            await HttpContext.Authentication.ChallengeAsync(Startup.SignInPolicyId, authenticationProperties);
+        }
+
+        [HttpGet]
+        public async Task ResetPassword()
+        {
+            var authenticationProperties = new AuthenticationProperties { RedirectUri = "/" };
+            await HttpContext.Authentication.ChallengeAsync(Startup.ResetPasswordPolicyId, authenticationProperties);
+        }
+
+        [HttpGet]
+        public async Task EditProfile(string redirectUri = "/")
+        {
+            //if (User != null && User.Identity.IsAuthenticated)
+            //{
+            //    // get policy 
+            //    var policy = User.FindFirst(Startup.AcrClaimType).Value.ToLowerInvariant();
+
+            //    await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //    await HttpContext.Authentication.SignOutAsync(policy);
+
+            //    HttpContext.Response.Redirect("/Account/EditProfile?redirecturi=" + Uri.EscapeDataString(redirectUri));
+            //    return;
+            //}
+
+            var authenticationProperties = new AuthenticationProperties { RedirectUri = redirectUri };
+            await HttpContext.Authentication.ChallengeAsync(Startup.ProfilePolicyId, authenticationProperties);
+        }
+
+        [HttpGet]
+        public async Task SignOut()
+        {
+            var callbackUrl = Url.Action("SignedOut", "Account", values: null, protocol: Request.Scheme);
+
+            if (User.Identity.IsAuthenticated)
+            {
+
+                var authenticationProperties = new AuthenticationProperties { RedirectUri = callbackUrl };
+
+                // get policy 
+                var policy = User.FindFirst(Startup.AcrClaimType).Value.ToLowerInvariant();
+
+                await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.Authentication.SignOutAsync(policy, authenticationProperties);
+            }
+            else
+            {
+                HttpContext.Response.Redirect(callbackUrl);
+            }
+        }
+
+        public IActionResult SignedOut()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
+            return View();
+        }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        public const string AcrClaimType = "http://schemas.microsoft.com/claims/authnclassreference";
+        public static string ResetPasswordPolicyId;
+        public static string SignInPolicyId;
+        public static string ProfilePolicyId;
+        public static string ClientId;
+        public static string AadInstance;
+        public static string Tenant;
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -29,6 +43,9 @@ namespace WebApplication1
         {
             // Add framework services.
             services.AddMvc();
+
+            // Add Authentication services.
+            services.AddAuthentication(sharedOptions => sharedOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +64,25 @@ namespace WebApplication1
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            // Configure the OWIN pipeline to use cookie auth.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            // App config settings
+            ClientId = Configuration["AzureAd:ClientId"];
+            AadInstance = Configuration["AzureAd:AadInstance"];
+            Tenant = Configuration["AzureAd:Tenant"];
+
+            // B2C policy identifiers
+            SignInPolicyId = Configuration["AzureAd:Policies:SignIn"].ToLowerInvariant();
+            ResetPasswordPolicyId = Configuration["AzureAd:Policies:PasswordReset"].ToLowerInvariant();
+            ProfilePolicyId = Configuration["AzureAd:Policies:EditProfile"].ToLowerInvariant();
+
+
+            // Configure the OWIN pipeline to use OpenID Connect auth.
+            app.UseOpenIdConnectAuthentication(CreateOptionsFromPolicy(ResetPasswordPolicyId));
+            app.UseOpenIdConnectAuthentication(CreateOptionsFromPolicy(SignInPolicyId));
+            app.UseOpenIdConnectAuthentication(CreateOptionsFromPolicy(ProfilePolicyId));
+
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
@@ -55,6 +91,55 @@ namespace WebApplication1
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        OpenIdConnectOptions CreateOptionsFromPolicy(string policy)
+        {
+            return new OpenIdConnectOptions
+            {
+                // For each policy, give OWIN the policy-specific metadata address, and
+                // set the authentication type to the id of the policy
+                MetadataAddress = string.Format(AadInstance, Tenant, policy),
+                AuthenticationScheme = policy,
+                CallbackPath = new PathString(string.Format("/{0}", policy)),
+
+                // These are standard OpenID Connect parameters, with values pulled from config.json
+                ClientId = ClientId,
+                Events = new OpenIdConnectEvents
+                {
+                    OnRemoteFailure = RemoteFailure
+                },
+                ResponseType = OpenIdConnectResponseType.IdToken,
+
+                // This piece is optional - it is used for displaying the user's name in the navigation bar.
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                },
+            };
+        }
+
+        // Used for avoiding yellow-screen-of-death
+        Task RemoteFailure(FailureContext context)
+        {
+            context.HandleResponse();
+
+            // Check for password reset message
+            if (context.Failure.Message?.Contains("AADB2C90118") ?? false)
+            {
+                context.Response.Redirect("/Account/ResetPassword");
+            }
+            else if (context.Failure.Message?.Contains("AADB2C90091") ?? false)
+            {
+                // canceled edit profile
+                context.Response.Redirect("/");
+            }
+            else
+            {
+                context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+            }
+
+            return Task.FromResult(0);
         }
     }
 }
